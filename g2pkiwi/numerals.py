595e0e8b800582d1f9ecc54cf7e3402cac13e087
dowon <ks2515@naver.com>
2022-10-25 14:03:43 +0900

    feat: enhance processing numbers

--- g2pkiwi/numerals.py
+++ g2pkiwi/numerals.py
@@ -1,15 +1,22 @@
-# -*- coding: utf-8 -*-
 """
 https://github.com/kyubyong/g2pK
 """
-
 import re
 
 # This is a list of bound nouns preceded by pure Korean numerals.
 BOUND_NOUNS = "군데 권 개 그루 닢 대 두 마리 모 모금 뭇 발 발짝 방 번 벌 보루 살 수 술 시 쌈 움큼 정 짝 채 척 첩 축 켤레 톨 통"
 
+digits = "123456789"
+names = "일이삼사오육칠팔구"
+digit2name = {d: n for d, n in zip(digits, names)}
 
-def process_num(num, sino=True):
+modifiers = "한 두 세 네 다섯 여섯 일곱 여덟 아홉"
+decimals = "열 스물 서른 마흔 쉰 예순 일흔 여든 아흔"
+digit2mod = {d: mod for d, mod in zip(digits, modifiers.split())}
+digit2dec = {d: dec for d, dec in zip(digits, decimals.split())}
+
+
+def process_num(num: str, sino: bool = True) -> str:
     """Process a string looking like arabic number.
     num: string. Consists of [0-9,]. e.g., 12,345
     sino: boolean. If True, sino-Korean numerals, i.e., 일, 이, .. are considered.
@@ -21,25 +28,17 @@ def process_num(num, sino=True):
     >>> process_num("123,456,789", sino=False)
     일억이천삼백사십오만육천칠백여든아홉
     """
-    num = re.sub(",", "", num)
+    num = num.replace(",", "")
 
     if num == "0":
         return "영"
     if not sino and num == "20":
         return "스무"
 
-    digits = "123456789"
-    names = "일이삼사오육칠팔구"
-    digit2name = {d: n for d, n in zip(digits, names)}
-
-    modifiers = "한 두 세 네 다섯 여섯 일곱 여덟 아홉"
-    decimals = "열 스물 서른 마흔 쉰 예순 일흔 여든 아흔"
-    digit2mod = {d: mod for d, mod in zip(digits, modifiers.split())}
-    digit2dec = {d: dec for d, dec in zip(digits, decimals.split())}
-
     spelledout = []
     for i, digit in enumerate(num):
         i = len(num) - i - 1
+        name = None
         if sino:
             if i == 0:
                 name = digit2name.get(digit, "")
@@ -51,6 +50,7 @@ def process_num(num, sino=True):
                 name = digit2mod.get(digit, "")
             elif i == 1:
                 name = digit2dec.get(digit, "")
+
         if digit == "0":
             if i % 4 == 0:
                 last_three = spelledout[-min(3, len(spelledout)) :]
@@ -94,7 +94,19 @@ def process_num(num, sino=True):
             name = digit2name.get(digit, "") + "백"
         elif i == 15:
             name = digit2name.get(digit, "") + "천"
-        spelledout.append(name)
+        elif i == 16:
+            name = digit2name.get(digit, "") + "경"
+        elif i == 17:
+            name = digit2name.get(digit, "") + "십"
+        elif i == 18:
+            name = digit2name.get(digit, "") + "백"
+        elif i == 19:
+            name = digit2name.get(digit, "") + "천"
+
+        if name is not None:
+            spelledout.append(name)
+        else:
+            raise ValueError(f"digit is too long: {num}")
 
     return "".join(elem for elem in spelledout)
 
@@ -107,7 +119,7 @@ def convert_num(string):
     global BOUND_NOUNS
 
     # Bound Nouns
-    tokens = set(re.findall("([\d][\d,]*)([ㄱ-힣]+)/B", string))
+    tokens = set(re.findall(r"([\d][\d,]*)([ㄱ-힣]+)/B", string))
     for token in tokens:
         num, bn = token
         if bn in BOUND_NOUNS:
@@ -116,7 +128,10 @@ def convert_num(string):
             spelledout = process_num(num, sino=True)
         string = string.replace(f"{num}{bn}/B", f"{spelledout}{bn}/B")
 
-    # digit by digit for remaining digits
+    # remain digits
+    string = re.sub(r"\d[\d,]*\d", lambda m: process_num(m.group(), sino=True), string)
+
+    # digit by digit for still remaining digits
     digits = "0123456789"
     names = "영일이삼사오육칠팔구"
     for d, n in zip(digits, names):
